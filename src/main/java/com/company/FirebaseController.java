4c795bf897f6a6cd76f516fe76115974338cab95
mr SIMPle <151757347+staszekDaszek@users.noreply.github.com>
2024-01-11 08:30:51 +0100

    Add files via upload

---
+++ src/main/java/com/company/FirebaseController.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+public class FirebaseController {
+    private static final String FILE_NAME = "./dylanek-f3c0a-firebase-adminsdk-nqmpx-009809ccd0.json";
+    private static final String DATABASE_NAME = "https://dylanek-f3c0a-default-rtdb.europe-west1.firebasedatabase.app/";
+    private static final String URL = "/scores";
+
+    private static FirebaseDatabase db;
+
+    public static void init() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream(FILE_NAME);
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl(DATABASE_NAME)
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        db = FirebaseDatabase.getInstance();
+    }
+
+    public static void add(Score score) {
+        DatabaseReference reference = db.getReference(URL);
+        reference.child(score.getName()).setValueAsync(score);
+    }
+    //meow
+    /*public static void add(List<Score> scores) {
+        HashMap<String, Score> map = new HashMap<>();
+        for (int i = 0; i < scores.size(); i++) {
+            map.put(scores.get(i).getName(), scores.get(i));
+        }
+        DatabaseReference reference = db.getReference(URL);
+        reference.setValueAsync(map);
+    }*/
+
+    public static void getScores() {
+        DatabaseReference reference = db.getReference(URL);
+        reference.orderByKey().addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Score score = dataSnapshot.getValue(Score.class);
+                System.out.println("received: " + score);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("error");
+            }
+        });
+    }
+
+}
+
---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FirebaseController.init();
+
+
+        AnchorPane root = new AnchorPane();
+
+        TextField name = new TextField("");
+        name.setFont(Font.font(20));
+        name.setPromptText("Name:");
+
+
+        TextField score  = new TextField("");
+        score.setPromptText("Score:");
+        score.setFont(Font.font(20));
+
+        Button add = new Button("Add");
+        add.setFont(Font.font(20));
+        add.setOnAction(event -> {
+            if (! name.getText().isEmpty() &&! score.getText().isEmpty()){
+                Score score1 = new Score();
+                score1.setName(name.getText());
+                score1.setPoints(Integer.parseInt(score.getText()));
+                FirebaseController.add(score1);
+               
+            }
+        });
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(100);
+        hBox.setLayoutX(100);
+        hBox.setLayoutY(100);
+        hBox.getChildren().addAll(name, score, add);
+
+        root.getChildren().addAll(hBox);
+        Scene scene = new Scene(root, 1200, 1000);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
